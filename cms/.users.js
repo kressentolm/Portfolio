91a6d01991727fcc05e04a2fe9f58f4b766b8566
Chris Blackmon <gossblackmon@gmail.com>
2018-06-09 20:30:41 -0500

    Initial commit

---
+++ cms/.users.js
@@ -0,0 +1,11 @@
+({
+	users: [
+		{
+			username: 'cblackmon',
+			tags: [],
+			salt: 'cf5bd6a2f6cd29047bb3c4b94d9aa86f',
+			hash: '76f66792e72895ca7bc10b0bb78db74cd8a541cc72d431d9170f1fed2c6e00d0',
+			user_created_timestamp: 1528593628510
+		}
+	]
+})
